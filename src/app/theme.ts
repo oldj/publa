ce0b63ac42991244392593286e0571c26ff67bef
oldj <oldj.wu@gmail.com>
2026-04-07 15:10:51 +0800

    style(theme): 调整 Mantine 主题的默认字号，让后台的字号稍大一些。

--- src/app/theme.ts
+++ src/app/theme.ts
@@ -2,6 +2,13 @@ import { createTheme } from '@mantine/core'
 
 export const theme = createTheme({
   primaryColor: 'dark',
+  fontSizes: {
+    xs: '12px',
+    sm: '14px',
+    md: '16px',
+    lg: '18px',
+    xl: '20px',
+  },
   components: {
     Badge: {
       styles: {
