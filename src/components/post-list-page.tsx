382f4b676ca272ab320176b4787befba342e9266
oldj <oldj.wu@gmail.com>
2026-04-08 12:13:51 +0800

    fix: 修正文章列表页“阅读全文”链接的样式问题
    调整链接容器的显示方式为flex以修复对齐问题，移除冗余字体大小定义，并重命名图标类名以保持一致性

--- src/components/post-list-page.tsx
+++ src/components/post-list-page.tsx
@@ -51,7 +51,7 @@ const PostCard = (props: { post: IPost }) => {
         <div className="post-list-summary">{post.html}</div>
         <Link href={post.url} className="post-list-read-more">
           <span>阅读全文</span>
-          <span className="post-list-icon">
+          <span className="post-list-read-more-icon">
             <IconChevronRight size={16} />
           </span>
         </Link>
